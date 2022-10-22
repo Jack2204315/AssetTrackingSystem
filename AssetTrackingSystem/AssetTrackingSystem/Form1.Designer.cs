cb595ba6d5b8817bbce53ce6350eb62bb6eee784
Jack2204315 <2204315@uad.ac.uk>
2022-10-22 20:14:42 +0100

    Creating the baseline data fields on the windows form for inserting new data. Testing different code for establishing connection to mySQL database.

--- AssetTrackingSystem/AssetTrackingSystem/Form1.Designer.cs
+++ AssetTrackingSystem/AssetTrackingSystem/Form1.Designer.cs
@@ -29,13 +29,148 @@ namespace AssetTrackingSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.syname = new System.Windows.Forms.Label();
+            this.ip = new System.Windows.Forms.Label();
+            this.type = new System.Windows.Forms.Label();
+            this.manfac = new System.Windows.Forms.Label();
+            this.mod = new System.Windows.Forms.Label();
+            this.sysname = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // syname
+            // 
+            this.syname.AutoSize = true;
+            this.syname.Location = new System.Drawing.Point(23, 118);
+            this.syname.Name = "syname";
+            this.syname.Size = new System.Drawing.Size(72, 13);
+            this.syname.TabIndex = 0;
+            this.syname.Text = "System Name";
+            this.syname.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ip
+            // 
+            this.ip.AutoSize = true;
+            this.ip.Location = new System.Drawing.Point(37, 222);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(58, 13);
+            this.ip.TabIndex = 1;
+            this.ip.Text = "IP Address";
+            this.ip.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // type
+            // 
+            this.type.AutoSize = true;
+            this.type.Location = new System.Drawing.Point(64, 196);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(31, 13);
+            this.type.TabIndex = 2;
+            this.type.Text = "Type";
+            // 
+            // manfac
+            // 
+            this.manfac.AutoSize = true;
+            this.manfac.Location = new System.Drawing.Point(25, 170);
+            this.manfac.Name = "manfac";
+            this.manfac.Size = new System.Drawing.Size(70, 13);
+            this.manfac.TabIndex = 3;
+            this.manfac.Text = "Manufacturer";
+            // 
+            // mod
+            // 
+            this.mod.AutoSize = true;
+            this.mod.Location = new System.Drawing.Point(59, 144);
+            this.mod.Name = "mod";
+            this.mod.Size = new System.Drawing.Size(36, 13);
+            this.mod.TabIndex = 4;
+            this.mod.Text = "Model";
+            // 
+            // sysname
+            // 
+            this.sysname.Location = new System.Drawing.Point(101, 115);
+            this.sysname.Name = "sysname";
+            this.sysname.Size = new System.Drawing.Size(247, 20);
+            this.sysname.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(101, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(101, 167);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(247, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(101, 141);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(247, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(101, 253);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(246, 28);
+            this.btn_insert.TabIndex = 10;
+            this.btn_insert.Text = "Insert new asset data";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.sysname);
+            this.Controls.Add(this.mod);
+            this.Controls.Add(this.manfac);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.ip);
+            this.Controls.Add(this.syname);
+            this.Name = "Form1";
+            this.Text = "Asset Tracking System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label syname;
+        private System.Windows.Forms.Label ip;
+        private System.Windows.Forms.Label type;
+        private System.Windows.Forms.Label manfac;
+        private System.Windows.Forms.Label mod;
+        private System.Windows.Forms.TextBox sysname;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btn_insert;
     }
 }
 
--- AssetTrackingSystem/AssetTrackingSystem/Form1.cs
+++ AssetTrackingSystem/AssetTrackingSystem/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;  // for MySQL
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace AssetTrackingSystem
 {
     public partial class Form1 : Form
@@ -15,6 +17,43 @@ namespace AssetTrackingSystem
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //This is my connection string i have assigned the database file address path
+                string MyConnection2 = "datasource=lochnagar.abertay.ac.uk;port=22;username=sql2204315;password=45Hysx3CEDI4";
+                //This is my insert query in which i am taking input from the user through windows forms
+                string Query = "insert into student.studentinfo(idStudentInfo,Name,Father_Name,Age,Semester) values('" + this.IdTextBox.Text + "','" + this.NameTextBox.Text + "','" + this.FnameTextBox.Text + "','" + this.AgeTextBox.Text + "','" + this.SemesterTextBox.Text + "');";
+                //This is  MySqlConnection here i have created the object and pass my connection string.
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                //This is command class which will handle the query and connection object.
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();     // Here our query will be executed and data saved into the database.
+                MessageBox.Show("Save Data");
+                while (MyReader2.Read())
+                {
+                }
+                MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
