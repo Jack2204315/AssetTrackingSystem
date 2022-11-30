10142bd6e2e433d994bdc99b990f5f5fa036e9ad
Jack2204315 <2204315@uad.ac.uk>
2022-11-30 22:13:41 +0000

    Delete data function - fully functional. Added Automatic hardware data retrieval - partial functionality. Automatic processor ID retrieval test - successfully.

---
+++ AssetTrackingSystem/AssetTrackingSystem/HardwareInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+public static class HardwareInfo
+{
+    ///
+    /// Retrieving Processor Id.
+    /// 
+    /// 
+    /// 
+    public static String GetProcessorId()
+    {
+
+        ManagementClass mc = new ManagementClass("win32_processor");
+        ManagementObjectCollection moc = mc.GetInstances();
+        String Id = String.Empty;
+        foreach (ManagementObject mo in moc)
+        {
+
+            Id = mo.Properties["processorID"].Value.ToString();
+            break;
+        }
+        return Id;
+
+    }
+}
--- AssetTrackingSystem/AssetTrackingSystem/Program.cs
+++ AssetTrackingSystem/AssetTrackingSystem/Program.cs
@@ -17,6 +17,8 @@ namespace AssetTrackingSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new deletebtn());
+
         }
+
     }
 }
